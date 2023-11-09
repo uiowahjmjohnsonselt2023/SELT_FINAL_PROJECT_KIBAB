13a87efe6b5885f2f33a28db6e60408ecc77fb8f
kmackin <kailey-mackin@uiowa.edu>
2023-11-08 19:16:16 -0600

    plz send help :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,27 @@
 class User < ActiveRecord::Base
+
+
   before_save {|user| user.email=user.email.downcase}
+  has_secure_password
   #before_save :create_session_token
-  validates :name, presence: true, length: {maximum: 50}
+  validates :first_name, presence: true, length: {maximum: 50}
+  validates :last_name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.\z/i
   validates :email, presence: true,
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum:6}
+  validates :password_confirmation, presence: true
 
   #private
   #def create_session_token
   #  self.session_token = SecureRandom.urlsafe_base64
   #end
+  def self.view_all_users
+    all_users = User.all
+
+    all_users.each do |user|
+      puts "Item ID: #{user.user_id}, Name: #{user.name}, Email: #{user.email}"
+    end
+  end
 end
