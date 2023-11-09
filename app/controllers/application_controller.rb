4f0e8658ed2952c5a8f0e89c69143f8913f92255
kmackin <kailey-mackin@uiowa.edu>
2023-11-08 19:54:51 -0600

    adding login...?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   protected
   def set_current_user
       @current_user ||= User.find_by_session_token(session[:session_token])
-      redirect_to login_path_url unless @current_user
+      redirect_to login_path unless @current_user
   end
 
   def current_user?(id)
-    @current_user.user_id.to_s == id
+    @current_user.id.to_s == id
   end
 end
