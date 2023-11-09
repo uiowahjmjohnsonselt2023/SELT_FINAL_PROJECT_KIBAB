b1913d307eee75f8d9dca96491bcec8fd89cf7ad
kmackin <kailey-mackin@uiowa.edu>
2023-11-08 21:52:56 -0600

    quick comment on migration which helps make more sense not :user_id col here just :id

--- db/migrate/20231109015805_add_session_token_to_users.rb
+++ db/migrate/20231109015805_add_session_token_to_users.rb
@@ -1,6 +1,6 @@
 class AddSessionTokenToUsers < ActiveRecord::Migration
   def change
     add_column :users, :session_token, :string
-    add_index :users, :session_token
+    add_index :users, :session_token ## this creates :id column in params NOT user_id
   end
 end
