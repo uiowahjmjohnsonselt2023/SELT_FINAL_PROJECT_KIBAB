baf8a57e99368539f1ffc9984176b0402c0c6e97
Brandon Cano <brandoncano01@gmail.com>
2023-11-09 20:09:18 -0600

    removed the dollar sign from the regex

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,17 +6,13 @@ class Product < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   validates :category, presence: true, length: {maximum: 50}
   validates :description, presence: true, :allow_blank => true # Default product blank, can change later
-  VALID_PRICE_REGEX = /\$\d+()|(.\d\d)/
+  VALID_PRICE_REGEX = /\d+()|(.\d\d)/
   validates :price, presence: true, format: {with: VALID_PRICE_REGEX} # Regex for US dollar format
   validates :location, presence: true # Formatting may be needed in the future
   validates :user_email, presence: true
 
   def set_user_email(email)
     self.user_email = email
-  end
-
-  def set_product_as_sold
-    self.is_sold? = true
   end
 
   # Searches database for specified product name, can return multiple products
