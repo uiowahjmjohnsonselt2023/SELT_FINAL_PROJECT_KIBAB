bf0d708828173f7ef3eaef1ed836a3301d47ec66
kmackin <kailey-mackin@uiowa.edu>
2023-11-10 00:55:34 -0600

    updating product_spec for updated db stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,8 @@ class Product < ActiveRecord::Base
 
   validates :name, presence: true, length: {maximum: 50}
   validates :category, presence: true, length: {maximum: 50}
-  validates :description, presence: true, :allow_blank => true # Default product blank, can change later
+  validates :description, presence: true
+  #TODO: Fix regex expression controlling price, add validation location, description, and categories
   VALID_PRICE_REGEX = /\d+()|(.\d\d)/
   validates :price, presence: true, format: {with: VALID_PRICE_REGEX} # Regex for US dollar format
   validates :location, presence: true # Formatting may be needed in the future
