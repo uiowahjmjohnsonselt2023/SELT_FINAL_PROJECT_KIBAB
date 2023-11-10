72eee32e1623c0b2d83c17bd38e2890901825660
kmackin <kailey-mackin@uiowa.edu>
2023-11-10 14:08:10 -0600

    changed some steps to user_steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -33,12 +33,6 @@ When(/^I am not logged in as: "(.*)"$/) do |email|
   expect(@user).to be_nil
 end
 
-Then(/^The following data shall be displayed: (.*)$/) do |user_info|
-  user_info.split(', ').each do |user|
-    expect(page).to have_content(user)
-  end
-end
-
 Then /^I should not see the user page for "(.*)" with "(.*)" and password "(.*)"$/ do |email1, email2, password|
   visit login_path_url
   fill_in 'Email', :with => email2
@@ -47,9 +41,5 @@ Then /^I should not see the user page for "(.*)" with "(.*)" and password "(.*)"
   @user_id = User.find_by_email(email1).user_id
   visit user_path(@user_id)
   expect(page).not_to have_content("John Jones")
-end
-
-Then /No data shall be displayed/ do
-  expect(page).to have_content("Please Login to view Information")
 end
 
