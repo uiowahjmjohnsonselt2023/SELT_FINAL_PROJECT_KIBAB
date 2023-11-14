7180d9e1506cc8eecf76388e41713d7d0b927d0c
Brandon Cano <brandoncano01@gmail.com>
2023-11-13 21:22:10 -0600

    removed HABTM relationships as we no longer need them

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Product < ActiveRecord::Base
-  has_and_belongs_to_many :users
 
   validates :name, presence: true, length: {maximum: 50}
   validates :category, presence: true, length: {maximum: 50}
