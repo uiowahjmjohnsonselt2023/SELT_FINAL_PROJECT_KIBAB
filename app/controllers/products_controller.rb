11d3ceb13a97eb98895eec2c9a4623a70cf11345
kmackin <kailey-mackin@uiowa.edu>
2023-11-19 20:44:53 -0600

    removing testing comments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,6 @@
 class ProductsController < ApplicationController
   before_filter :set_current_user
 
-  ## Test
-  # tet2
   def product_params
     params.require(:product).permit(:name,:image,:category,:description,:price,:location,:is_sold?)
   end
