5e88ea3f4a0cd7c5f8ac082e3c86129e15b75170
kmackin <kailey-mackin@uiowa.edu>
2023-11-19 19:52:00 -0600

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_filter :set_current_user
 
+  ## Test
   def product_params
     params.require(:product).permit(:name,:image,:category,:description,:price,:location,:is_sold?)
   end
