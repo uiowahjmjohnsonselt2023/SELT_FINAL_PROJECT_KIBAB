15995afc7009c6c0f5ac86d4f77dc477372cafdd
Ian Kuk <ikuk@uiowa.edu>
2023-11-20 19:53:27 -0600

    makes search feature show products if the name matches but the filters dont

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,9 +22,9 @@ class ProductsController < ApplicationController
     elsif params[:search] == "" && params[:product][:categories].present? && params[:product][:descriptions].present?
       @products = Product.filtered_search('',params[:product][:categories], params[:product][:descriptions]).where(is_sold: false)
     end
-    if @products.present? == false
-      # flash[:notice] = "No products match your search try something else"
-      @products = Product.where(is_sold: false)
+    if @products.empty?
+      flash[:notice] = "No products match your search here are some close results"
+      @products = Product.filtered_search(params[:search],"None", "None").where(is_sold: false)
     end
   end
 
