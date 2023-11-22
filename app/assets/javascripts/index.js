465d1e46f25db99aa3782610830ff2a56d10b4ad
kmackin <kailey-mackin@uiowa.edu>
2023-11-22 12:45:54 -0600

    now tracking the classes appropriately. Just next to figure out how to move them.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,52 +4,46 @@ console.log(document.title)
 
 function prevImg() {
     console.log("prev")
-    const slides = document.querySelectorAll("carousel-slides")
+    const slides = document.querySelectorAll(".carousel-slide")
+    console.log(slides)
     offset = -1
-    const activeSlide = document.querySelector("carousel-slides active")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    let newIndex = 0;
+    let activeSlide;
+    slides.forEach(
+        function(node, index) {
+            if (node.classList.contains("active")) {
+                newIndex = index + offset;
+                activeSlide = node;
+            }
+        }
+    );
 
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+    if (newIndex < 0) newIndex = slides.length - 1
+    if (newIndex >= slides.length) newIndex = 0
 
-    slides.children[newIndex].classList.add("active")
     activeSlide.classList.remove("active")
+    slides[newIndex].classList.add("active")
 }
 
 function nextImg() {
     console.log("next")
-    const slides = document.querySelectorAll("carousel-slides")
+    const slides = document.querySelectorAll(".carousel-slide")
+    console.log(slides)
     offset = 1
-    const activeSlide = document.querySelector("carousel-slides active")
-    console.log(slides.children)
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    let activeSlide;
+    slides.forEach(
+        function(node, index) {
+            if (node.classList.contains("active")) {
+                newIndex = index + offset;
+                activeSlide = node;
+            }
+        }
+    );
 
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].classList.add("active")
+    if (newIndex < 0) newIndex = slides.length - 1
+    if (newIndex >= slides.length) newIndex = 0
+
     activeSlide.classList.remove("active")
+    slides[newIndex].classList.add("active")
 }
-
-// buttons.forEach(button => {
-//     button.addEventListener("click", () => {
-//         console.log("entered")
-//         offset =  0
-//         if (button.classList.contains("next")) {
-//             offset = 1
-//         }
-//         else  {
-//             offset = -1
-//         }
-//         const slides = button.closest("carousel").querySelector("carousel-slides")
-//
-//         const activeSlide = slides.querySelector("carousel-slide active")
-//         let newIndex = [...slides.children].indexOf(activeSlide) + offset
-//
-//         if (newIndex < 0) newIndex = slides.children.length - 1
-//         if (newIndex >= slides.children.length) newIndex = 0
-//
-//         slides.children[newIndex].classList.add("active")
-//         activeSlide.classList.remove("active")
-//     })
-// })
