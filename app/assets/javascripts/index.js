80216ae59cfdb32be14c9a2a13534d80d1b0b910
kmackin <kailey-mackin@uiowa.edu>
2023-11-22 14:05:09 -0600

    image carousel is kinda working!

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,25 +1,27 @@
-console.log("hello")
-buttons = document.querySelectorAll("button.carousel-button")
-console.log(document.title)
-
 function prevImg() {
     console.log("prev")
     const slides = document.querySelectorAll(".carousel-slide")
+    const slidesContainer = document.querySelector(".carousel-slides")
     console.log(slides)
     offset = -1
     let newIndex = 0;
     let activeSlide;
+    let width;
     slides.forEach(
         function(node, index) {
             if (node.classList.contains("active")) {
                 newIndex = index + offset;
                 activeSlide = node;
+                width = activeSlide.clientWidth;
             }
         }
     );
 
     if (newIndex < 0) newIndex = slides.length - 1
     if (newIndex >= slides.length) newIndex = 0
+
+    slidesContainer.style.transform = 'translateX(' + -newIndex*width + 'px)'
+    console.log("translate")
 
     activeSlide.classList.remove("active")
     slides[newIndex].classList.add("active")
@@ -28,21 +30,26 @@ function prevImg() {
 function nextImg() {
     console.log("next")
     const slides = document.querySelectorAll(".carousel-slide")
+    const slidesContainer = document.querySelector(".carousel-slides")
     console.log(slides)
     offset = 1
     let activeSlide;
+    let width;
     slides.forEach(
         function(node, index) {
             if (node.classList.contains("active")) {
                 newIndex = index + offset;
                 activeSlide = node;
+                width = activeSlide.clientWidth;
             }
         }
     );
 
-
     if (newIndex < 0) newIndex = slides.length - 1
     if (newIndex >= slides.length) newIndex = 0
+
+    slidesContainer.style.transform = 'translateX(' + -newIndex*width + 'px)'
+    console.log("translate")
 
     activeSlide.classList.remove("active")
     slides[newIndex].classList.add("active")
