7ca8ad57f4e23d132ebc3ea9e9cf0ba344f6cbd9
Ian Kuk <ikuk@uiowa.edu>
2023-12-03 20:00:43 -0600

    enabled the smarty streets flag

--- app/models/product.rb
+++ app/models/product.rb
@@ -37,27 +37,27 @@ class Product < ActiveRecord::Base
   end
 
   def self.valid_address(city,state,address,zip)
-    run_it = false
+    run_it = true
     if run_it
-    client = SmartyStreetsConfig.client
-    lookup = SmartyStreets::USStreet::Lookup.new
-    lookup.street = address
-    lookup.state = state
-    lookup.city = city
-    lookup.zipcode = zip
-    lookup.candidates = 3
-    lookup.match = SmartyStreets::USStreet::MatchType::STRICT
-    begin
-      client.send_lookup(lookup)
-    rescue SmartyStreets::SmartyError => err
-      result = "Got the error" + err.to_s
-      return result
-    end
-    if lookup.result.empty?
-      false
-    else
-      true
-    end
+      client = SmartyStreetsConfig.client
+      lookup = SmartyStreets::USStreet::Lookup.new
+      lookup.street = address
+      lookup.state = state
+      lookup.city = city
+      lookup.zipcode = zip
+      lookup.candidates = 3
+      lookup.match = SmartyStreets::USStreet::MatchType::STRICT
+      begin
+        client.send_lookup(lookup)
+      rescue SmartyStreets::SmartyError => err
+        result = "Got the error" + err.to_s
+        return result
+      end
+      if lookup.result.empty?
+        false
+      else
+        true
+      end
     end
   end
 
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,27 +3,27 @@ class Purchase < ActiveRecord::Base
   belongs_to :product
 
   def self.valid_address(city,state,address,zip)
-    run_it = false
+    run_it = true
     if run_it
-    client = SmartyStreetsConfig.client
-    lookup = SmartyStreets::USStreet::Lookup.new
-    lookup.street = address
-    lookup.state = state
-    lookup.city = city
-    lookup.zipcode = zip
-    lookup.candidates = 3
-    lookup.match = SmartyStreets::USStreet::MatchType::STRICT
-    begin
-      client.send_lookup(lookup)
-    rescue SmartyStreets::SmartyError => err
-      result = "Got the error" + err.to_s
-      return result
-    end
-    if lookup.result.empty?
-      false
-    else
-      true
-    end
+      client = SmartyStreetsConfig.client
+      lookup = SmartyStreets::USStreet::Lookup.new
+      lookup.street = address
+      lookup.state = state
+      lookup.city = city
+      lookup.zipcode = zip
+      lookup.candidates = 3
+      lookup.match = SmartyStreets::USStreet::MatchType::STRICT
+      begin
+        client.send_lookup(lookup)
+      rescue SmartyStreets::SmartyError => err
+        result = "Got the error" + err.to_s
+        return result
+      end
+      if lookup.result.empty?
+        false
+      else
+        true
+      end
     end
    end
 end
