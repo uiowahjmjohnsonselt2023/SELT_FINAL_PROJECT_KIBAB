c1ce2ae2b591e5921cc6b462dcf6485891b22b5b
Ian Kuk <ikuk@uiowa.edu>
2023-12-03 20:14:47 -0600

    stupid thang got deleted

--- config/initializers/smartystreets.rb
+++ config/initializers/smartystreets.rb
@@ -1,7 +1,7 @@
 
 SmartyStreetsConfig = OpenStruct.new(
   key: ENV['SMARTY_CLIENT_KEY'],
-  referer: ENV['']
+  referer: ENV['SMARTY_CLIENT_HOST']
 )
 SmartyStreetsConfig.credentials = SmartyStreets::SharedCredentials.new(SmartyStreetsConfig.key, SmartyStreetsConfig.referer)
 SmartyStreetsConfig.client = SmartyStreets::ClientBuilder.new(SmartyStreetsConfig.credentials).with_licenses(['us-core-cloud']).build_us_street_api_client
