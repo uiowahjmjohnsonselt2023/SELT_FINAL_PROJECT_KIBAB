fdaf7c934be619b2edcd4cb00bfa2faf042a710d
Brendan Suter <brendansuter02@gmail.com>
2023-12-04 18:02:08 -0600

    Added migration changes to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_005930) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_235743) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_005930) do
     t.string "street_address"
     t.string "zip"
     t.boolean "is_sold", default: false
+    t.integer "product_traffic"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
