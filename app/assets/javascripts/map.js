4f676c7c34dd5ca3cf144b592a56da936c15d76c
kmackin <kailey-mackin@uiowa.edu>
2023-12-04 19:07:39 -0600

    added marker for products

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,11 +6,18 @@ function initMap() {
         lng: parseFloat(coords.getAttribute("data-longitude"))
     };
 
+    const product_name = coords.getAttribute("data-name")
     // (document.getElementById("product_map")
 
     const map = new google.maps.Map(document.getElementById("product_map"), {
-        zoom: 10,
+        zoom: 15,
         center: product,
+    });
+
+     new google.maps.Marker({
+        position: product,
+        map,
+        title: product_name,
     });
 }
 
